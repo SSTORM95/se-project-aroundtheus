2a4aa4a3ac33087723c49f03c9a88bc780ab53bc
daniel sida <banuelossidadaniel@gmail.com>
2024-05-04 16:31:42 -0700

    Project 4 Final Stage

--- scripts/index.js
+++ scripts/index.js
@@ -24,15 +24,58 @@ const initialCards = [
         link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg',
     },
 ]
+// Elements //
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector('#profile-edit-modal');
 const modalCloseBtn = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector(".modal__form-title");
+const profileDescriptionInput = document.querySelector(".modal__form-description");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".gallery__cards");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
+// Function //
+
+function closePopUp(){
+    profileEditModal.classList.remove('modal__opened');
+}
+
+function getCardElement(data) {
+
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector('.card__image');
+    const cardTitleEl = cardElement.querySelector('.card__description-title');
+    cardTitleEl.textContent = data.name;
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    return cardElement;
+}
+
+// Event handlers // 
+
+function handleProfileEditSubmit(e){
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent =profileDescriptionInput.value;
+    closePopUp();
+}
+
+// Event listener//
 
 profileEditBtn.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal__opened");
 })
 
-modalCloseBtn.addEventListener("click", () => {
-    profileEditModal.classList.remove('modal__opened');
+modalCloseBtn.addEventListener("click", closePopUp)
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((data) => {
+    const cardElement = getCardElement(data);
+    cardListEl.prepend(cardElement);
 })
